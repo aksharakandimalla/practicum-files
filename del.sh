d83d3ad1b0663d010c9a2d33f8e4f8177dacb701
aksharakandimalla <aksharakandimalla95@gmail.com>
2021-07-09 18:08:59 +0100

    Create del.sh

---
+++ del.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+
+
+
+rm *_step_10000.pt
+#rm *_step_20000.pt
+#rm *_step_30000.pt
+rm *_step_40000.pt
+rm *_step_50000.pt
+rm *_step_60000.pt
+rm *_step_70000.pt
+rm *_step_80000.pt
+rm *_step_90000.pt
+rm *_step_100000.pt
+rm *_step_110000.pt
+rm *_step_120000.pt
+rm *_step_130000.pt
+rm *_step_140000.pt
+rm *_step_150000.pt
+
